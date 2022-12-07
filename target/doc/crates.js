1ec1f0c4a4de1176d4ae0dfa9702b29deb266c1f
motazmo <brandonsusini@gmail.com>
2022-12-07 13:59:56 -0500

    Deleted main.rs.original. Re-generated Documentation.

--- target/doc/crates.js
+++ target/doc/crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["blackjack","cfg_if","getrandom","libc","ppv_lite86","rand","rand_chacha","rand_core"];
+window.ALL_CRATES = ["blackjack","cfg_if","getrandom","ppv_lite86","rand","rand_chacha","rand_core"];
--- target/doc/source-files.js
+++ target/doc/source-files.js
@@ -1,10 +1,10 @@
-var sourcesIndex = {};
-sourcesIndex["blackjack"] = {"name":"","files":["main.rs"]};
-sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["getrandom"] = {"name":"","files":["error.rs","error_impls.rs","lib.rs","macos.rs","use_file.rs","util.rs","util_libc.rs"]};
-sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"bsd","dirs":[{"name":"apple","dirs":[{"name":"b64","dirs":[{"name":"aarch64","files":["align.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["mod.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
-sourcesIndex["ppv_lite86"] = {"name":"","files":["generic.rs","lib.rs","soft.rs","types.rs"]};
-sourcesIndex["rand"] = {"name":"","dirs":[{"name":"distributions","files":["bernoulli.rs","distribution.rs","float.rs","integer.rs","mod.rs","other.rs","slice.rs","uniform.rs","utils.rs","weighted.rs","weighted_index.rs"]},{"name":"rngs","dirs":[{"name":"adapter","files":["mod.rs","read.rs","reseeding.rs"]}],"files":["mock.rs","mod.rs","std.rs","thread.rs"]},{"name":"seq","files":["index.rs","mod.rs"]}],"files":["lib.rs","prelude.rs","rng.rs"]};
-sourcesIndex["rand_chacha"] = {"name":"","files":["chacha.rs","guts.rs","lib.rs"]};
-sourcesIndex["rand_core"] = {"name":"","files":["block.rs","error.rs","impls.rs","le.rs","lib.rs","os.rs"]};
+var sourcesIndex = JSON.parse('{\
+"blackjack":["",[],["main.rs"]],\
+"cfg_if":["",[],["lib.rs"]],\
+"getrandom":["",[],["error.rs","error_impls.rs","lib.rs","util.rs","windows.rs"]],\
+"ppv_lite86":["",[["x86_64",[],["mod.rs","sse2.rs"]]],["lib.rs","soft.rs","types.rs"]],\
+"rand":["",[["distributions",[],["bernoulli.rs","distribution.rs","float.rs","integer.rs","mod.rs","other.rs","slice.rs","uniform.rs","utils.rs","weighted.rs","weighted_index.rs"]],["rngs",[["adapter",[],["mod.rs","read.rs","reseeding.rs"]]],["mock.rs","mod.rs","std.rs","thread.rs"]],["seq",[],["index.rs","mod.rs"]]],["lib.rs","prelude.rs","rng.rs"]],\
+"rand_chacha":["",[],["chacha.rs","guts.rs","lib.rs"]],\
+"rand_core":["",[],["block.rs","error.rs","impls.rs","le.rs","lib.rs","os.rs"]]\
+}');
 createSourceSidebar();
